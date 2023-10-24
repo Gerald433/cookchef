828dce0f5406e5ecb18b6e99f61554d633a0eb35
Gérald Gaillard <gerald8142@gmail.com>
2023-10-24 11:53:47 +0200

    Create the HeaderMenuXs component

---
+++ src/components/HeaderMenuXs.js
@@ -0,0 +1,12 @@
+import styles from "./HeaderMenuXs.module.scss";
+
+function HeaderMenuXs() {
+  return (
+    <ul className={`${styles.MenuContainer}  card p-20`}>
+      <li>WishList</li>
+      <li>Connexion</li>
+    </ul>
+  );
+}
+
+export default HeaderMenuXs;
