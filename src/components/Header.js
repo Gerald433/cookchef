b89828b71003b60a023591c56ec77876b1da9ec2
Gérald Gaillard <gerald8142@gmail.com>
2023-10-24 11:00:11 +0200

    Adapt the menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import styles from "./Header.module.scss";
 import logo from "../assets/images/logo.jpg";
 
-
 function Header() {
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center `}>
-      <i class="fa-solid fa-bars mr-15"></i>
+      
       <div className="flex-fill">
         <img src={logo} alt="logo du site" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i class="fa-sharp fa-solid fa-basket-shopping mr-5"></i>
-          <span>Panier</span>
+          <i class="fa-sharp fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i class={`${styles.headerXs} fa-solid fa-bars`}></i>
     </header>
   );
 }
