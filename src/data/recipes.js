012e6a7057640847fcd9ed0c4a167b26705b6e28
Gérald Gaillard <gerald8142@gmail.com>
2023-10-25 09:57:49 +0200

    Update recipe.js

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -2,41 +2,41 @@ export const data = [
   {
     title: "Jambon Champignon",
     image: "assets/images/jambon_champignon.jpg",
-    id: "Aze8poIiD3g",
+    _id: "Aze8poIiD3g",
   },
   {
     title: "4 Fromages",
     image: "assets/images/4fromages.jpg",
-    id: "AcA8moIiD3g",
+    _id: "AcA8moIiD3g",
   },
   {
     title: "Campagnarde",
     image: "assets/images/campagnarde.jpg",
-    id: "Y6OgisiGBjM",
+    _id: "Y6OgisiGBjM",
   },
   {
     title: "Chorizo",
     image: "assets/images/Chorizo.jpg",
-    id: "Y64gasaGBpp",
+    _id: "Y64gasaGBpp",
   },
   {
     title: "Royale",
     image: "assets/images/laRoyale.jpg",
-    id: "Y99gasaGFpp",
+    _id: "Y99gasaGFpp",
   },
   {
     title: "Provençale",
     image: "assets/images/provencale.jpg",
-    id: "R88gasUEFpp",
+    _id: "R88gasUEFpp",
   },
   {
     title: "Vegan",
     image: "assets/images/vegan.jpg",
-    id: "R9PRZsaGFOO",
+    _id: "R9PRZsaGFOO",
   },
   {
     title: "Chèvre",
     image: "assets/images/chevre.jpg",
-    id: "L2GGZsaFEOO",
+    _id: "L2GGZsaFEOO",
   },
 ];
