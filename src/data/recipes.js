81db4f597a226f94d87330f025af9f80294a8896
Gérald Gaillard <gerald8142@gmail.com>
2023-10-26 14:28:32 +0200

    Configure a post request on server api

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -2,41 +2,41 @@ export const data = [
   {
     title: "Jambon Champignon",
     image: "assets/images/jambon_champignon.jpg",
-    _id: "Aze8poIiD3g",
+
   },
   {
     title: "4 Fromages",
     image: "assets/images/4fromages.jpg",
-    _id: "AcA8moIiD3g",
+
   },
   {
     title: "Campagnarde",
     image: "assets/images/campagnarde.jpg",
-    _id: "Y6OgisiGBjM",
+
   },
   {
     title: "Chorizo",
     image: "assets/images/Chorizo.jpg",
-    _id: "Y64gasaGBpp",
+
   },
   {
     title: "Royale",
     image: "assets/images/laRoyale.jpg",
-    _id: "Y99gasaGFpp",
+
   },
   {
     title: "Provençale",
     image: "assets/images/provencale.jpg",
-    _id: "R88gasUEFpp",
+
   },
   {
     title: "Vegan",
     image: "assets/images/vegan.jpg",
-    _id: "R9PRZsaGFOO",
+
   },
   {
     title: "Chèvre",
     image: "assets/images/chevre.jpg",
-    _id: "L2GGZsaFEOO",
+
   },
 ];
