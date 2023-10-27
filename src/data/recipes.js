aa359b549e67d599f0fe345703361bce0d0df14f
Gérald Gaillard <gerald8142@gmail.com>
2023-10-27 03:55:56 +0200

    update recipe

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -2,41 +2,33 @@ export const data = [
   {
     title: "Jambon Champignon",
     image: "assets/images/jambon_champignon.jpg",
-
   },
   {
     title: "4 Fromages",
     image: "assets/images/4fromages.jpg",
-
   },
   {
     title: "Campagnarde",
     image: "assets/images/campagnarde.jpg",
-
   },
   {
     title: "Chorizo",
-    image: "assets/images/Chorizo.jpg",
-
+    image: "assets/images/chorizo.jpg",
   },
   {
     title: "Royale",
     image: "assets/images/laRoyale.jpg",
-
   },
   {
     title: "Provençale",
     image: "assets/images/provencale.jpg",
-
   },
   {
     title: "Vegan",
     image: "assets/images/vegan.jpg",
-
   },
   {
     title: "Chèvre",
     image: "assets/images/chevre.jpg",
-
   },
 ];
