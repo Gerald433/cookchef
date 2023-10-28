da974f466037453d06dc3eed16e2c9b16026d9a4
Gérald Gaillard <gerald8142@gmail.com>
2023-10-28 12:07:42 +0200

    Update Rooter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import Content from "./pages_Homepage/Content";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import styles from "./App.module.scss";
 // import { seedRecipes } from "./data/seed";
 
@@ -9,9 +9,9 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
-      <Header />
+      {/* <Header /> */}
       <Content />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from 'react-dom';
+import { ReactDOM } from "react-dom";
 import "./assets/styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Acceuil from "./pages_Homepage/Acceuil";
 
+const root = createRoot(document.getElementById('root'));
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" element={<Acceuil />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    
+
+    </main>
+    
+    <Footer />
   </BrowserRouter>
 );
 
