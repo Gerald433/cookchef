18bc38db85a7bd7d02b1418de25f096811155472
Gérald Gaillard <gerald8142@gmail.com>
2023-10-30 17:17:27 +0100

    Set up a pageLoader with operating logic

---
+++ src/components/PageLoader/PageLoader.js
@@ -0,0 +1,12 @@
+import styles from "./PageLoader.module.scss";
+
+function PageLoader() {
+  return (
+    <div className={`${styles.load}`}>
+      <div className={`${styles.loading}`}></div>
+      <p className={`${styles.load_text}`}>Aux fourneaux</p>
+    </div>
+  );
+}
+
+export default PageLoader;
