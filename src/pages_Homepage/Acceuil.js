fc9e362c3302d15c9367751534c17acb71b610b1
Gérald Gaillard <gerald8142@gmail.com>
2023-10-30 11:00:44 +0100

    Update home page

--- src/pages_Homepage/Acceuil.js
+++ src/pages_Homepage/Acceuil.js
@@ -1,7 +1,18 @@
 import styles from "./Acceuil.module.scss";
+import { Link } from "react-router-dom";
 
 function Acceuil() {
-  return <h2>page acceuil</h2>;
+  return (
+    <div>
+      <img
+        className={`${styles.imgAccueil}`}
+        src="assets/images/imgAccueil.jpg"
+        alt="Fromage étiré d'une part de pizza"
+      />
+      <h2>page acceuil</h2>
+      <Link to="/app"> viens manger </Link>
+    </div>
+  );
 }
 
 export default Acceuil;
