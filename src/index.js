0d934b5e492a74bb28e95fd0ea4b2d5235116787
Gérald Gaillard <gerald8142@gmail.com>
2023-10-31 08:05:24 +0100

    Edit route logic

--- src/index.js
+++ src/index.js
@@ -3,28 +3,43 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ReactDOM } from "react-dom";
 import "./assets/styles/index.scss";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Acceuil from "./pages_Homepage/Acceuil";
+import Content from "./pages_Homepage/Content";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <Header />
     <main>
       <Routes>
-        <Route path="/" element={<Acceuil />} />
-        <Route path="/app" element={<App />} />
+        {/* Utilisation de la prop element pour conditionner le rendu du Header */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <Acceuil />
+              <Footer />
+            </div>
+          }
+        />
+        <Route
+          path="/recipes"
+          element={
+            <div>
+              <Header />
+              <Content />
+              <Footer />
+            </div>
+          }
+        />
       </Routes>
     </main>
-    <Footer />
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
