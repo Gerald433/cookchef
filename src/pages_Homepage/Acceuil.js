d25808d448a50d90adb08df86e74c50032a3c7aa
Gérald Gaillard <gerald8142@gmail.com>
2023-10-31 06:29:09 +0100

    Set up the advertising text

--- src/pages_Homepage/Acceuil.js
+++ src/pages_Homepage/Acceuil.js
@@ -1,4 +1,3 @@
-
 import styles from "./Acceuil.module.scss";
 import { Link } from "react-router-dom";
 
@@ -14,6 +13,13 @@ function Acceuil() {
 
         <div>
           <h1>Street - Pizza</h1>
+          <p className={`${styles.planning}`}>
+            A Sainte Sigolène, Les samedi et dimanche,
+            <br /> sur le parking de Carrefour Market.
+            <br />
+            16h30 - 21h00
+          </p>
+          <span className={`${styles.priceIndice}`}>10 à 13 euros</span>
           <Link className={`${styles.linkPizzasPage}`} to="/app">
             Recettes
           </Link>
