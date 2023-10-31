867b9b4893ef00f90f8680e0721e5853a6a4870a
Gérald Gaillard <gerald8142@gmail.com>
2023-10-31 09:38:27 +0100

    Restructuring Recipe

--- src/pages_Homepage/components/Recipe/Recipe.js
+++ src/pages_Homepage/components/Recipe/Recipe.js
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
-function Recipe({ recipe: { _id, like, title, image }, updateLike }) {
+function Recipe({ recipe: { _id, like, title, image, price }, updateLike }) {
   const handleClick = () => {
     updateLike(_id, !like);
   };
@@ -11,11 +11,20 @@ function Recipe({ recipe: { _id, like, title, image }, updateLike }) {
       <div className={styles.imageContainer}>
         <img src={image} alt={title} className={`${styles.imgRecipe} `} />
       </div>
+
       <div
         className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
-        <h3 className="mb-10">{title}</h3>
-        <i className={`fa-solid fa-heart ${like ? "text-primary" : ""}`}></i>
+        <div>
+          <h3 className="mb-10">{title}</h3>
+        </div>
+
+        <div
+          className={`${styles.under} d-flex justify-content-center align-items-center`}
+        >
+          <i className={`fa-solid fa-heart ${like ? "text-primary" : ""}`}></i>
+          <p className={`${styles.price} `}>{price}</p>
+        </div>
       </div>
     </div>
   );
