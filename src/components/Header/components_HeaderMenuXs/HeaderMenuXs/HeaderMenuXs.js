bcfc44f4018b85718ee0f2a8bb9c688150e60fcb
Gérald Gaillard <gerald8142@gmail.com>
2023-11-01 11:55:15 +0100

    Update style header mobile

--- src/components/Header/components_HeaderMenuXs/HeaderMenuXs/HeaderMenuXs.js
+++ src/components/Header/components_HeaderMenuXs/HeaderMenuXs/HeaderMenuXs.js
@@ -1,9 +1,27 @@
 import styles from "./HeaderMenuXs.module.scss";
+import { useLocation, Link } from "react-router-dom";
+
 
 function HeaderMenuXs() {
+
+  const location = useLocation();
+  let linkPath = "/wishlist"; // par défaut, le lien pointe vers la page Wishlist
+  let buttonText = "Wishlist"; // Texte par défaut pour le bouton
+
+  if (location.pathname === "/wishlist") {
+    linkPath = "/recipes"; // Changer le lien pour la page Wishlist
+    buttonText = "Recettes"; // Changer le texte pour la page Wishlist
+  }
   return (
     <ul className={`${styles.MenuContainer}  card p-20`}>
-      <li>WishList</li>
+      
+      <li><Link className={`${styles.wishList}`} to={linkPath}>
+            <span>{buttonText}</span>
+          </Link></li>
+
+
+
+      
       <li>Connexion</li>
     </ul>
   );
