ce316ca79e97255847781ba9ef5844b5e9eeccb8
Gérald Gaillard <gerald8142@gmail.com>
2023-11-01 15:26:01 +0100

    Format the price calculation form

--- src/pages_Homepage/WishContent.js
+++ src/pages_Homepage/WishContent.js
@@ -1,30 +1,60 @@
-import React from "react";
-import styles from './WishContent.module.scss'
+import React, { useState } from "react";
+import styles from "./WishContent.module.scss";
 
 function WishContent() {
   // Récupérer la liste de souhaits depuis localStorage
-  const wishList = JSON.parse(localStorage.getItem('wishList')) || [];
+  const wishList = JSON.parse(localStorage.getItem("wishList")) || [];
+
+  const storedQuantities = JSON.parse(localStorage.getItem("quantities")) || {};
+  const [quantities, setQuantities] = useState(storedQuantities);
+
+  // Mettre à jour la quantité sélectionnée pour une recette
+  const handleQuantityChange = (event, recipeId) => {
+    const { value } = event.target;
+    const newQuantities = { ...quantities, [recipeId]: value };
+    setQuantities({ ...quantities, [recipeId]: value });
+    localStorage.setItem("quantities", JSON.stringify(newQuantities));
+  };
+
+  const calculateTotal = (recipe) => {
+    const quantity = parseInt(quantities[recipe._id]) || 0;
+    const price = parseFloat(recipe.price) || 0;
+    return (quantity * price).toFixed(2);
+  };
 
   // Afficher la liste de souhaits
   return (
     <div>
       <h2>Ma liste de souhaits</h2>
-      <ul className={`${styles.listTotal}`}>
-        {wishList.map((recipe) => (
-          <li key={recipe._id}>
-            {recipe.title} <span>{recipe.price}</span>
-            {/* Afficher d'autres détails de la recette si nécessaire */}
-          </li>
-        ))}
-      </ul>
+      <form
+        action="POST"
+        className="d-flex align-items-center justify-content-center"
+      >
+        <ul className={`${styles.listTotal}`}>
+          {wishList.map((recipe) => (
+            <li key={recipe._id}>
+              {recipe.title} <br />
+              {/* Afficher d'autres détails de la recette si nécessaire */}
+              <div className={`${styles.firstCalcul}`}>
+                <input
+                  value={quantities[recipe._id] || 0}
+                  onChange={(event) => handleQuantityChange(event, recipe._id)}
+                  className={`${styles.quantitySelect}`}
+                  type="number"
+                />
+                <span className={`${styles.priceUnit}`}>x</span>
+                <span className={`${styles.priceUnit}`}>{recipe.price}</span>
+                <span className={`${styles.priceUnit}`}>=</span>
+                <span className={`${styles.total}`}>
+                  {calculateTotal(recipe) + ' €'}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
 
 export default WishContent;
-
-
-
-
-
-
