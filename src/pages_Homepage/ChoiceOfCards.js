fe736c428c33f08898dc0ba9b9bd55d6c8394375
Gérald Gaillard <gerald8142@gmail.com>
2023-11-07 17:45:45 +0100

    Add minimal style

--- src/pages_Homepage/ChoiceOfCards.js
+++ src/pages_Homepage/ChoiceOfCards.js
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 function AChoiceOfCards() {
   return (
     <>
-      <div className={`${styles.globalBox} d-flex container`}>
-        <Link to="/recipes" className={`${styles.cutInThree}`}>
+      <div className={`${styles.globalBox}   d-flex container`}>
+        <Link to="/recipes" className={`${styles.cutInThree}  ${styles.left}`}>
           <div>
             <h2>Pizzas</h2>
           </div>
         </Link>
-        <Link className={`${styles.cutInThree}`}>
+        <Link className={`${styles.cutInThree}  ${styles.middleBox} `}>
           <div>
             <h2>Boissons</h2>
           </div>
         </Link>
-        <Link className={`${styles.cutInThree}`}>
+        <Link className={`${styles.cutInThree}  ${styles.right} `}>
           <div>
             <h2>Desserts</h2>
           </div>
