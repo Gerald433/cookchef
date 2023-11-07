d1082aa2fed2985956fe608b506e2885f3fbec73
Gérald Gaillard <gerald8142@gmail.com>
2023-11-07 19:08:21 +0100

    Update ChoiceOfCards

--- src/pages_Homepage/ChoiceOfCards.js
+++ src/pages_Homepage/ChoiceOfCards.js
@@ -6,17 +6,32 @@ function AChoiceOfCards() {
     <>
       <div className={`${styles.globalBox}   d-flex container`}>
         <Link to="/recipes" className={`${styles.cutInThree}  ${styles.left}`}>
-          <div>
-            <h2>Pizzas</h2>
+          <div className={`${styles.divison}`}>
+            <img
+              className={`${styles.pictureChoice}   d-flex`}
+              src="assets/images/pizzaChoice.jpg"
+              alt="pizza de présentation"
+            />
+            <h2 className={`${styles.titleBox}   d-flex container`}>Pizzas</h2>
           </div>
         </Link>
         <Link className={`${styles.cutInThree}  ${styles.middleBox} `}>
-          <div>
+          <div className={`${styles.divison}`}>
+            <img
+              className={`${styles.pictureChoice}   d-flex`}
+              src="assets/images/sodaChoice.jpg"
+              alt="pizza de présentation"
+            />
             <h2>Boissons</h2>
           </div>
         </Link>
         <Link className={`${styles.cutInThree}  ${styles.right} `}>
-          <div>
+          <div className={`${styles.divison}`}>
+            <img
+              className={`${styles.pictureChoice}   d-flex`}
+              src="assets/images/dessertChoice.jpg"
+              alt="pizza de présentation"
+            />
             <h2>Desserts</h2>
           </div>
         </Link>
