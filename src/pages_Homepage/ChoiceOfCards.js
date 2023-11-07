8799cf83a095017e3b7ed3b98e4a433cc0ed745e
Gérald Gaillard <gerald8142@gmail.com>
2023-11-07 17:09:31 +0100

    Add ChoiceOfCards.module.scss

---
+++ src/pages_Homepage/ChoiceOfCards.js
@@ -0,0 +1,32 @@
+import styles from "./Acceuil.module.scss";
+import { Link } from "react-router-dom";
+
+function Acceuil() {
+  return (
+    <>
+      <div className={`${styles.container} container`}>
+        <img
+          className={`${styles.imgAccueil}`}
+          src="assets/images/imgAccueil.jpg"
+          alt="Fromage étiré d'une part de pizza"
+        />
+
+        <div>
+          <h1>Street - Pizza</h1>
+          <p className={`${styles.planning}`}>
+            A Sainte Sigolène, Les samedi et dimanche,
+            <br /> sur le parking de Carrefour Market.
+            <br />
+            16h30 - 21h00
+          </p>
+          {/* <span className={`${styles.priceIndice}`}>10 à 13 euros</span> */}
+          <Link className={`${styles.linkPizzasPage}`} to="/recipes">
+            Notre carte
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Acceuil;
