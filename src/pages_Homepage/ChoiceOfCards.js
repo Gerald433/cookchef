ac5a6097f72b1b88bb758e5fcce155c79d009b3f
Gérald Gaillard <gerald8142@gmail.com>
2023-11-10 11:35:09 +0100

    Put images in full screen

--- src/pages_Homepage/ChoiceOfCards.js
+++ src/pages_Homepage/ChoiceOfCards.js
@@ -5,34 +5,34 @@ function AChoiceOfCards() {
   return (
     <>
       <div className={`${styles.globalBox}   d-flex container`}>
-        <Link to="/recipes" className={`${styles.cutInThree}  ${styles.left}`}>
-          <div className={`${styles.divison}`}>
+        <Link to="/recipes" className={`${styles.cutInThree}  ${styles.left} `}>
+          <div className={`${styles.part}  d-flex flex-column`}>
             <img
               className={`${styles.pictureChoice}   d-flex`}
               src="assets/images/pizzaChoice.jpg"
               alt="pizza de présentation"
             />
-            <h2 className={`${styles.titleBox}   d-flex container`}>Pizzas</h2>
+            <h2 className={`${styles.titleBox}   d-flex `}>Pizzas</h2>
           </div>
         </Link>
         <Link className={`${styles.cutInThree}  ${styles.middleBox} `}>
-          <div className={`${styles.divison}`}>
+          <div className={`${styles.part} `}>
             <img
               className={`${styles.pictureChoice}   d-flex`}
               src="assets/images/sodaChoice.jpg"
               alt="pizza de présentation"
             />
-            <h2>Boissons</h2>
+            <h2 className={`${styles.titleBox}   d-flex `}>Boissons</h2>
           </div>
         </Link>
         <Link className={`${styles.cutInThree}  ${styles.right} `}>
-          <div className={`${styles.divison}`}>
+          <div className={`${styles.part}`}>
             <img
               className={`${styles.pictureChoice}   d-flex`}
               src="assets/images/dessertChoice.jpg"
               alt="pizza de présentation"
             />
-            <h2>Desserts</h2>
+            <h2 className={`${styles.titleBox}   d-flex `}>Desserts</h2>
           </div>
         </Link>
       </div>
