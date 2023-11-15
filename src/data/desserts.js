95074d7661eec27927226903569750bbb243b93f
Gérald Gaillard <gerald8142@gmail.com>
2023-11-15 15:21:31 +0100

    Add cookies

---
+++ src/data/desserts.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    title: "2 Macarons chocolat",
+    image: "assets/images/macaronChocolat.jpg",
+    like: false,
+    price: 3,
+    _id: 100,
+  },
+  {
+    title: " 2 Macarons Fraise",
+    image: "assets/images/macaronFraise.jpg",
+    like: false,
+    price: 3,
+    _id: 200,
+  },
+  {
+    title: "Donut",
+    image: "assets/images/donut.jpg",
+    like: false,
+    price: 2,
+    _id: 300,
+  },
+  {
+    title: "Perrier",
+    image: "assets/images/perrier.jpg",
+    like: false,
+    price: 2,
+    _id: 400,
+  },
+  {
+    title: "Red bull",
+    image: "assets/images/redBull.jpg",
+    like: false,
+    price: 2,
+    _id: 700,
+  },
+  {
+    title: "Heineken",
+    image: "assets/images/heineken.jpg",
+    like: false,
+    price: 3,
+    _id: 800,
+  },
+];
