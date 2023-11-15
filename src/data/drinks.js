db8262a0648e2ddd504f02e5cb7e321e7ab990c1
Gérald Gaillard <gerald8142@gmail.com>
2023-11-15 14:07:00 +0100

    Make a data drinks

---
+++ src/data/drinks.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    title: "Coca-Cola",
+    image: "assets/images/coca.jpg",
+    like: false,
+    price: 2,
+    _id: 10,
+  },
+  {
+    title: "Coca zero",
+    image: "assets/images/cocaZero.jpg",
+    like: false,
+    price: 2,
+    _id: 20,
+  },
+  {
+    title: "Schweppes",
+    image: "assets/images/schweppes.jpg",
+    like: false,
+    price: 2,
+    _id: 30,
+  },
+  {
+    title: "Perrier",
+    image: "assets/images/perrier.jpg",
+    like: false,
+    price: 2,
+    _id: 40,
+  },
+  {
+    title: "Red bull",
+    image: "assets/images/redBull.jpg",
+    like: false,
+    price: 2,
+    _id: 70,
+  },
+  {
+    title: "Heineken",
+    image: "assets/images/chevre.jpg",
+    like: false,
+    price: 3,
+    _id: 80,
+  },
+];
